b1763ce2267ec011e8496c63261a9e631e60d3ff
Myles <myles.rangel@gmail.com>
2019-11-02 20:04:32 -0700

    Added more test cases to globalWarming.js

--- src/JavaScript/Random/globalWarming.js
+++ src/JavaScript/Random/globalWarming.js
@@ -10,3 +10,30 @@
 
 
 
+function GlobalWarming(arr){
+    let maxTemp = 0;
+    console.log(maxTemp);
+
+    //do magic here
+    for(let i = 0; i<arr.length - 1; i++){
+        if(arr[i + 1] - arr[i] > maxTemp){
+            maxTemp = arr[i+1] - arr[i];
+        }
+        if(arr[i+2] - arr[i] > maxTemp ){
+            maxTemp = arr[i+2] - arr[i];
+        }
+    }
+    //if the maxtemp never goes up return 0
+    if(maxTemp < 0){
+        return 0;
+    }
+
+    return maxTemp;
+}
+
+console.log(GlobalWarming([2,32,10,15,27,36,50,53]));
+console.log(GlobalWarming([22,32,60,15,27,33,50,53]));
+console.log(GlobalWarming([22,52,18,15,27,33,20,53]));
+console.log(GlobalWarming([22,12,10,15,27,19,20,53]));
+console.log(GlobalWarming([53,48,28,22,20,18,5,3]));
+console.log(GlobalWarming([-3,-12,-19,-20,-23,-30,-45]));
