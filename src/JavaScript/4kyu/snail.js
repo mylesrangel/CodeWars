2fc224fe5a65f2240defa1d46971f56a20f49838
Myles <myles.rangel@gmail.com>
2019-11-21 00:17:48 -0800

    BREAK: Not Finished snail

---
+++ src/JavaScript/4kyu/snail.js
@@ -0,0 +1,55 @@
+
+
+// Snail Sort
+// Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+// array = [[1,2,3],
+//          [4,5,6],
+//          [7,8,9]]
+// snail(array) #=> [1,2,3,6,9,8,7,4,5]
+// For better understanding, please follow the numbers of the next array consecutively:
+
+// array = [[1,2,3],
+//          [8,9,4],
+//          [7,6,5]]
+// snail(array) #=> [1,2,3,4,5,6,7,8,9]
+// This image will illustrate things more clearly:
+
+
+// NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+
+// NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array [[]].
+
+
+
+snail = function(array) {
+    // enjoy
+    let snailArray = [];
+    console.log(typeof(array))
+    console.log(array)
+    console.log(array.length)
+    console.log(array[3])
+
+    //add all the elements in array[0] into snailArray
+    for(let i = 0; i<array[0].length; i++){
+        snailArray.push(array[0][i]);
+    }
+    //add all the elements at the end of each array element (array[element])
+    for(let n = 0; n<array.length; n++){
+        console.log(array[0].length)
+        console.log(array[n][2])
+        snailArray.push(array[n][array.length-1]);
+    }
+    console.log(snailArray)
+
+
+
+}
+
+
+
+//console.log(snail([[]]), []);
+//console.log(snail([[1]]), [1]);
+ console.log(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), [1, 2, 3, 6, 9, 8, 7, 4, 5]);
+// console.log(snail([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]), [1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13]);
+// console.log(snail([[1, 2, 3, 4, 5, 6], [20, 21, 22, 23, 24, 7], [19, 32, 33, 34, 25, 8], [18, 31, 36, 35, 26, 9], [17, 30, 29, 28, 27, 10], [16, 15, 14, 13, 12, 11]]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]);
