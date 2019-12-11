86686ca23faa1669728e7659190d3bf3f1d22014
Myles <myles.rangel@gmail.com>
2019-12-10 21:17:43 -0800

    Finished Iq Test

--- src/JavaScript/6kyu/iqTest.js
+++ src/JavaScript/6kyu/iqTest.js
@@ -11,46 +11,68 @@
 
 function iqTest(numbers){
     
-    let oddValueFound = false;
-    let evenValueFound = false;
+    let oddValueFound = 0;
+    let oddValueIndex = 0;
+    let evenValueFound = 0;
+    let evenValueIndex = 0;
+
 
     //place a space at the beginning of the string
-    console.log(typeof(numbers))
-    console.log(numbers);
-    console.log(numbers = numbers.split(' ').join(""));
-    console.log(typeof(numbers));
-    console.log(numbers);
-
-    numbers = " "+numbers;
-
-    //find what we are looking for (even or odd)
-    if(numbers[1] % 2 == 0){
-        evenValueFound = true;
-    }else{
-        oddValueFound = true;
-    }
-
-    console.log(numbers[2])
-    console.log(numbers[5] % 2)
+    numbers = numbers.split(' ');
 
     //loop through the array to looking for odd / even values
-    for(var i = 2; i<numbers.length; i++){
+    for(var i = 0; i<numbers.length; i++){
 
         //if the number is even and we already found an odd
-        if(numbers[i] % 2 == 0 && oddValueFound){
-            return i;
-        }
-        //if the number is odd and we already found an even;
-        else if(numbers[i] % 2 == 1 && evenValueFound){
-            return i;
+        if(numbers[i] % 2 == 0){
+            evenValueFound++;
+            evenValueIndex = i+1;
+        }else{
+            oddValueFound++;
+            oddValueIndex = i+1;
         }
     }
     //if nothing was found return the first value (that was the answer)
-    return 1;
+    if(oddValueFound == 1){
+        return oddValueIndex;
+    }else{
+        return evenValueIndex;
+    }
 }
 
 
-console.log(iqTest("2 4 7 8 10"),3);
- console.log(iqTest("1 2 2"), 1);
+//console.log(iqTest("2 4 7 8 10"),3);
+//console.log(iqTest("1 2 2"), 1);
 
 
+
+
+//BEST PRACTICE
+
+// function iqTest(numbers){
+//     numbers = numbers.split(" ").map(function(el){return parseInt(el)});
+    
+//     var odd = numbers.filter(function(el){ return el % 2 === 1});
+//     var even = numbers.filter(function(el){ return el % 2 === 0});
+    
+//     return odd.length < even.length ? (numbers.indexOf(odd[0]) + 1) : (numbers.indexOf(even[0]) + 1);
+//   }
+
+
+//ANOTHER ONE
+// function iqTest(numbers){
+//     numbers = numbers.split(' ')
+    
+//     var evens = []
+//     var odds = []
+    
+//     for (var i = 0; i < numbers.length; i++) {
+//       if (numbers[i] & 1) {
+//         odds.push(i + 1)
+//       } else {
+//         evens.push(i + 1)
+//       }
+//     }
+    
+//     return evens.length === 1 ? evens[0] : odds[0]
+//   }
