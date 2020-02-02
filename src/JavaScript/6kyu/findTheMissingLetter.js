d2789d588b21a2eebc13c3ce8875a846de8f655a
Myles <myles.rangel@gmail.com>
2020-02-02 00:28:55 -0800

    Finsihed

---
+++ src/JavaScript/6kyu/findTheMissingLetter.js
@@ -0,0 +1,106 @@
+// #Find the missing letter
+
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+
+// ['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+
+// ["a","b","c","d","f"] -> "e"
+// ["O","Q","R","S"] -> "P"
+// (Use the English alphabet with 26 letters!)
+
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+// I have also created other katas. Take a look if you enjoyed this kata!
+
+const theAlphabet = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26
+};
+
+const theNumbers = {
+    1: 'a',
+    2: 'b',
+    3: 'c',
+    4: 'd',
+    5: 'e',
+    6: 'f',
+    7: 'g',
+    8: 'h',
+    9: 'i',
+    10: 'j',
+    11: 'k',
+    12: 'l',
+    13: 'm',
+    14: 'n',
+    15: 'o',
+    16: 'p',
+    17: 'q',
+    18: 'r',
+    19: 's',
+    20: 't',
+    21: 'u',
+    22: 'v',
+    23: 'w',
+    24: 'x',
+    25: 'y',
+    26: 'z'
+};
+
+
+function findMissingLetter(array){
+
+    console.log(array);
+    console.log(theAlphabet['a']);
+
+    console.log(theAlphabet[array[0]])
+
+
+
+    let isUpperCase = array[0] === array[0].toString().toUpperCase();
+    let counter = theAlphabet[array[0].toLowerCase()];
+    
+    for(let i = 0; i<array.length; i++){
+        console.log(counter);
+        console.log(theAlphabet['a'])
+        if(counter != theAlphabet[array[i].toLowerCase()]){
+            if(isUpperCase){
+                return theNumbers[counter].toUpperCase();
+            }
+            return theNumbers[counter];
+        }
+        counter++;
+    }
+}
+
+
+// console.log(findMissingLetter(['a','b','c','d','f']), 'e');
+console.log(findMissingLetter(['O','Q','R','S']), 'P');
